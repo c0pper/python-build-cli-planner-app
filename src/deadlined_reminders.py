db143f26d862f0a7fefb1bc41d833bc7f770d96a
93simon <93simonster@gmail.com>
2022-12-30 15:14:00 +0100

    Build Your App

--- src/deadlined_reminders.py
+++ src/deadlined_reminders.py
@@ -15,18 +15,6 @@ class DeadlinedReminder(ABC, Iterable):
     def is_due(self):
         pass
 
-
-class DateReminder(DeadlinedReminder):
-    def __init__(self, text, date):
-        self.date = parse(date, dayfirst=True)
-        self.text = text
-
-    def is_due(self):
-        return self.date <= datetime.now()
-
-    def __iter__(self):
-        return iter([self.text, self.date.isoformat()])
-
     @classmethod
     def __subclasshook__(cls, subclass):
         if cls is not DeadlinedReminder:
@@ -39,3 +27,17 @@ class DateReminder(DeadlinedReminder):
             return NotImplemented
 
         return True
+
+
+class DateReminder(DeadlinedReminder):
+    def __init__(self, text, date):
+        self.date = parse(date, dayfirst=True)
+        self.text = text
+
+    def is_due(self):
+        return self.date <= datetime.now()
+
+    def __iter__(self):
+        return iter([self.text, self.date.isoformat()])
+
+
